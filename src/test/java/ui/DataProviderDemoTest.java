58e6735c0de8ed0654dbd8e96ee2b9c816e7e6f6
Cristian David <cristiandavid1291@gmail.com>
2022-04-11 15:24:36 -0500

    webportal\src\test\java\ui\DataProviderDemoTest2.java: dataProvider with web driver

--- src/test/java/ui/DataProviderDemoTest.java
+++ src/test/java/ui/DataProviderDemoTest.java
@@ -1,16 +1,28 @@
 package ui;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DataProviderDemoTest {
 	
 
 	
 	@Test(dataProvider = "data1")
-	public void test(String username,String password) {
+	public void test(String username,String password) throws InterruptedException {
 		
-		System.out.println(username +"============"+password);
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.id("user-name")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.id("login-button")).click();
+		driver.close();
 		
 	}
 	
@@ -19,37 +31,12 @@ public class DataProviderDemoTest {
 	public Object[][] dataSet1() {
 		
 		return new Object[][] {
-			{"user1","pass1"},
-			{"user2","pass2"},
-			{"user3","pass3"},
-			{"user4","pass4"}
+			{"standard_user","secret_sauce"},
+			{"performance_glitch_user","secret_sauce"},
+			{"problem_user","secret_sauce"},
+			{"locked_out_user","secret_sauce"}
 		};
 
 	}
-	
-	
-	@DataProvider
-	public Object[][] dataSet() {
-		
-		Object[][] dataset = new Object[4][2];
-		
-		//first row
-		dataset[0][0] = "user1";
-		dataset[0][1] = "pass1";
-		
-		//second row
-		dataset[1][0] = "user2";
-		dataset[1][1] = "pass2";
-		
-		//third row
-		dataset[2][0] = "user3";
-		dataset[2][1] = "pass3";
-		
-		//fourth row
-		dataset[3][0] = "user4";
-		dataset[3][1] = "pass4";
-		
-		return dataset;
-	}
-	
+
 }
